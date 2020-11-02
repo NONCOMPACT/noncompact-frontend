04fcbb79842d96341371ba6e3aa6d96aa621d60b
Jimmy Zhen <jzhen@stanford.edu>
2020-11-02 11:15:33 -0800

    Initial commit of institution and investigator metadata

---
+++ src/lib/institutions.js
@@ -0,0 +1,35 @@
+import LogoStanford from '../assets/logo-stanford.png';
+import LogoClevelandClinic from '../assets/logo-clevelandclinic.png';
+import LogoUPenn from '../assets/logo-upenn.png';
+import LogoErasmus from '../assets/logo-erasmus.png';
+import LogoSNUH from '../assets/logo-snuh.png';
+
+const institutions = [
+  {
+    name: 'Stanford University',
+    abbr: 'stanford',
+    logo: LogoStanford,
+  },
+  {
+    name: 'The Cleveland Clinic',
+    abbr: 'clevelandclinic',
+    logo: LogoClevelandClinic,
+  },
+  {
+    name: 'University of Pennsylvania',
+    abbr: 'upenn',
+    logo: LogoUPenn,
+  },
+  {
+    name: 'Erasmus Medical Center',
+    abbr: 'erasmus',
+    logo: LogoErasmus,
+  },
+  {
+    name: 'Seoul National University Hospital',
+    abbr: 'snuh',
+    logo: LogoSNUH,
+  },
+];
+
+export default institutions;
---
+++ src/lib/investigators.js
@@ -0,0 +1,94 @@
+import MattWheelerImg from '../assets/investigators/matt_wheeler.png';
+import KoenNiemanImg from '../assets/investigators/koen_nieman.png';
+import DanielRubinImg from '../assets/investigators/daniel_rubin.png';
+import FrancoisHaddadImg from '../assets/investigators/francois_haddad.png';
+import JuneRheeImg from '../assets/investigators/june_rhee.png';
+import WilsonTangImg from '../assets/investigators/wilson_tang.png';
+import DebbieKwonImg from '../assets/investigators/debbie_kwon.png';
+import AnjaliOwensImg from '../assets/investigators/anjali_owens.png';
+import HaroldLittImg from '../assets/investigators/harold_litt.png';
+import KadirCaliskanImg from '../assets/investigators/kadir_caliskan.png';
+import RicardoBuddeImg from '../assets/investigators/ricardo_budde.png';
+import AlexanderHirschImg from '../assets/investigators/alexander_hirsch.png';
+import SeungPyoLeeImg from '../assets/investigators/seung-pyo_lee.png';
+import WhalLeeImg from '../assets/investigators/whal_lee.png';
+
+const investigators = [
+  {
+    name: 'Matthew Wheeler',
+    photo: MattWheelerImg,
+    institution: 'Stanford University',
+  },
+  {
+    name: 'Koen Nieman',
+    photo: KoenNiemanImg,
+    institution: 'Stanford University',
+  },
+  {
+    name: 'Daniel Rubin',
+    photo: DanielRubinImg,
+    institution: 'Stanford University',
+  },
+  {
+    name: 'Francois Haddad',
+    photo: FrancoisHaddadImg,
+    institution: 'Stanford University',
+  },
+  {
+    name: 'June Rhee',
+    photo: JuneRheeImg,
+    institution: 'Stanford University',
+  },
+  {
+    name: 'Jarrett Rosenberg',
+    photo: null,
+    institution: 'Stanford University',
+  },
+  {
+    name: 'Wilson Tang',
+    photo: WilsonTangImg,
+    institution: 'The Cleveland Clinic',
+  },
+  {
+    name: 'Debbie Kwon',
+    photo: DebbieKwonImg,
+    institution: 'The Cleveland Clinic',
+  },
+  {
+    name: 'Anjali Owens',
+    photo: AnjaliOwensImg,
+    institution: 'University of Pennsylvania',
+  },
+  {
+    name: 'Harold Litt',
+    photo: HaroldLittImg,
+    institution: 'University of Pennsylvania',
+  },
+  {
+    name: 'Kadir Caliskan',
+    photo: KadirCaliskanImg,
+    institution: 'Erasmus Medical Center',
+  },
+  {
+    name: 'Ricardo Budde',
+    photo: RicardoBuddeImg,
+    institution: 'Erasmus Medical Center',
+  },
+  {
+    name: 'Alexander Hirsch',
+    photo: AlexanderHirschImg,
+    institution: 'Erasmus Medical Center',
+  },
+  {
+    name: 'Seung-Pyo Lee',
+    photo: SeungPyoLeeImg,
+    institution: 'Seoul National University Hospital',
+  },
+  {
+    name: 'Whal Lee',
+    photo: WhalLeeImg,
+    institution: 'Seoul National University Hospital',
+  },
+];
+
+export default investigators;
