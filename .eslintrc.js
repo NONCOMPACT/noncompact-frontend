bbf5a92c4b9acfddedec7a82bc15ab187e2a57b6
Jimmy Zhen <jzhen@stanford.edu>
2021-03-29 22:22:13 -0700

    ESLint+prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,16 @@
 module.exports = {
-  "extends": [
-      "airbnb",
-      "plugin:prettier/recommended",
-      "prettier",
-      "prettier/react"
-  ],
-  "plugins": [
-      "react-hooks"
-  ],
-  "rules": {
-      "jsx-a11y/label-has-for": "off",
-      "jsx-a11y/label-has-associated-control": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "import/no-extraneous-dependencies": [
-          "error",
-          {"devDependencies": true}
-      ],
-      "react/jsx-props-no-spreading": "off"
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  plugins: ['react-hooks'],
+  rules: {
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/jsx-props-no-spreading': 'off',
   },
-  "env": {
-      "browser": true,
-      "jest": true
-  }
+  env: {
+    browser: true,
+    jest: true,
+  },
 };
